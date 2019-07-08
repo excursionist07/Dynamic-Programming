852a8c94dcb052e65449ffb6d544bc5560527986
ShubhamKumar07 <49623544+ShubhamKumar07@users.noreply.github.com>
2019-07-08 11:50:45 +0530

    Add files via upload

---
+++ Longest Common Subsequence.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+#define mem(dp,a) memset(dp,a,sizeof dp)
+#define INF 1000000007
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define repb(i,a,b) for(ll i=a;i>=b;i--)
+#define f(n) for(ll i=0;i<n;i++)
+#define r(n) for(ll j=0;j<n;j++)
+#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
+using namespace std;
+typedef long long int ll;
+
+void lcs(string s1,string s2,ll m,ll n)
+{
+	ll k[m+1][n+1];
+	f(m+1)
+	{
+		r(n+1)
+		{
+			if(i==0 || j==0)
+				k[i][j]=0;
+			else if(s1[i-1]==s2[j-1])
+				k[i][j]=k[i-1][j-1]+1;
+			else
+				k[i][j]=max(k[i-1][j],k[i][j-1]);
+		}
+	}
+
+	/*string s3="";
+	//ll s3.length()=index;
+
+	ll i=m,j=n;
+	while(i>0 && j>0)
+    {
+        if(s1[i-1]==s2[j-1])
+        {
+
+         strcpy(s3,s1[i]);
+         i--;
+         j--;
+
+        }
+        else if(k[i-1][j]>k[i][j-1])
+           i--;
+        else
+           j--;
+    }
+    return s3;*/
+    if(k[m][n]!=0)
+    {
+    ll index=k[m][n];
+    char ch[index];
+    //ch[index]=' ';
+    ll i=m,j=n;
+    while(i>0 && j>0)
+    {
+        if(s1[i-1]==s2[j-1])
+        {
+
+         ch[index-1]=s1[i-1];
+         i--;
+         j--;
+         index--;
+
+        }
+        else if(k[i-1][j]>k[i][j-1])
+           i--;
+        else
+           j--;
+    }
+
+
+    f(k[m][n])
+      cout<<ch[i];
+    cout<<" "<<k[m][n]<<endl;
+    }
+
+    else
+    {
+        cout<<"no common thing"<<endl;
+    }
+
+
+}
+
+int main()
+{
+	hs;
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+	  string s1,s2;
+	  cin>>s1>>s2;
+	  ll a=s1.length();
+	  ll b=s2.length();
+	  lcs(s1,s2,a,b);
+
+	}
+	return 0;
+}
