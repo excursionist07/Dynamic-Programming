9916f56f2301815ee8a2b17d721ba82c5aa6b456
Shubham Kumar <49623544+ShubhamKumar07@users.noreply.github.com>
2019-07-14 12:44:31 +0530

    Update unbounded_knapsack.cpp

--- unbounded_knapsack.cpp
+++ unbounded_knapsack.cpp
@@ -1,38 +1,55 @@
+// same as rod cutting
+
 #include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof dp)
-#define INF 1000000007
+#define mem(dp,a) memset(dp,a,sizeof(dp))
+#define pb(x) push_back(x)
+#define m_p(x,y) make_pair(x,y)
 #define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repb(i,a,b) for(ll i=a;i>=b;i--)
+#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
 #define f(n) for(ll i=0;i<n;i++)
 #define r(n) for(ll j=0;j<n;j++)
+#define F first
+#define S second
+#define pi 3.14159265359
 #define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
 using namespace std;
 typedef long long int ll;
+ll HRX=1e18;
+ll INF=1e9+7;
 
-
-ll unbounded_knapsack(ll val[],ll wt[],ll n,ll w)
+void unbounded_kanpsack(ll wt[],ll val[],ll n,ll total)
 {
-	ll k[w+1];
-	memset(k,0,sizeof(k));
-	f(w+1)
-	{
-      r(n)
-      {
-      	if(wt[j]<=i)
-      		k[i]=max(k[i],val[j]+k[i-wt[j]]);
-      }
-	}
-	return k[w];
+  ll k[n+1][total+1];
+  f(n+1)
+  {
+    r(total+1)
+    {
+      if(i==0 || j==0)
+        k[i][j]=0;
+      else if(j>=wt[i-1])
+        k[i][j]=max(k[i-1][j],val[i-1]+k[i][j-wt[i-1]]);
+      else
+        k[i][j]=k[i-1][j];
+    }
+  }
+  cout<<k[n][total]<<endl;
 }
 int main()
 {
-	ll w,m;
-	cin>>w>>m;
-	ll wt[m],val[m];
-	f(m)
-	{
-		cin>>wt[i]>>val[i];
-	}
-	cout<<unbounded_knapsack(val,wt,m,w)<<endl;
-	return 0;
+  hs;
+  ll n;
+  cin>>n;
+  ll wt[n],val[n];
+  f(n)
+  {
+    cin>>wt[i]>>val[i];
+  }
+  ll total;
+  cin>>total;
+  unbounded_kanpsack(wt,val,n,total);
+  return 0;
 }
+
+/*
+https://practice.geeksforgeeks.org/problems/knapsack-with-duplicate-items/0
+*/
