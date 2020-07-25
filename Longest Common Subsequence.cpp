620d8f14c290b7f6b25efb728b4afb8e22a156c5
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-07-25 12:42:05 +0530

    Update Longest Common Subsequence.cpp

--- Longest Common Subsequence.cpp
+++ Longest Common Subsequence.cpp
@@ -9,6 +9,18 @@
 using namespace std;
 typedef long long int ll;
 
+ /* int lcs(string s1,string s2,int m,int n) ??Recursive
+    {
+     if(m==0 || n==0)
+         return 0;
+     else if(s1[m-1]==s2[n-1])
+         return 1+lcs(s1,s2,m-1,n-1);
+     else
+         return max(lcs(s1,s2,m-1,n),lcs(s1,s2,m,n-1));
+         
+    }*/
+    
+
 void lcs(string s1,string s2,ll m,ll n)
 {
 	ll k[m+1][n+1];
