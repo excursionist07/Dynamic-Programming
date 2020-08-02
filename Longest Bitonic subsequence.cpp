ffeb184695efbd4f01270a209f40959e1ab11b51
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-02 14:27:57 +0530

    Update Longest Bitonic subsequence.cpp

--- Longest Bitonic subsequence.cpp
+++ Longest Bitonic subsequence.cpp
@@ -1,49 +1,64 @@
-//8 Queen Problem - Recursive Backtracking
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-
-using namespace __gnu_pbds;
+#define mem(dp,a) memset(dp,a,sizeof(dp))
+#define pb(x) push_back(x)
+#define m_p(x,y) make_pair(x,y)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
+#define f(n) for(ll i=0;i<n;i++)
+#define r(n) for(ll j=0;j<n;j++)
+#define F first
+#define S second
+#define pi 3.14159265359
+#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
 using namespace std;
-typedef long long ll;
+typedef long long int ll;
+ll HRX=1e18;
+ll INF=1e9+7;
 
-#define mem(dp,a)           memset(dp,a,sizeof dp)
-#define rep(i,a,b)          for(ll i=a;i<b;i++)
-#define pb(x)               push_back(x)
-#define mp(x,y)             make_pair(x,y)
-#define fastio              ios_base::sync_with_stdio(false);cin.tie(NULL)
-#define F                   first
-#define S                   second
-#define all(v)              (v).begin(),(v).end()
-#define pi					3.14159265359
-ll INF=1e18+10;
-ll MOD=1000000007;
-
-const int N=8;
-int row[N];
-
-bool place(int r,int c)
-{
-	for(int prev=0;prev<c;prev++)
-      if(row[prev]==r || abs(row[prev]-r)==abs(prev-c))
-        return false;
-  	return true;
-}
-
-void backtrack(int c)
-{
-  	if(c==8)
-    {
-    	for(int i=0;i<8;i++)
-        	cout<<row[i]<<" ";
-      	cout<<"\n";
-    }
-    else
-  	for(int r=0;r<8;r++)
-      	if(place(r,c)){row[c]=r;backtrack(c+1);}
-}
 
 int main()
 {
-    //freopen("out.txt","w",stdout);
-  	backtrack(0);
+ hs;
+ ll t;
+ cin>>t;
+ f(t)
+ {
+  ll n;
+  cin>>n;
+  ll a[n],b[n],c[n];
+  f(n)
+  {
+   cin>>a[i];
+   b[i]=1;
+   c[i]=1;
+  }
+
+  for(ll j=1;j<n;j++)
+  {
+    for(ll i=0;i<j;i++)
+    {
+     if(a[j]>a[i])
+       b[j]=max(b[i]+1,b[j]);
+    }
+  }
+
+
+  for(ll j=n-2;j>=0;j--)
+  {
+    for(ll i=n-1;i>j;i--)
+    {
+     if(a[j]>a[i])
+      c[j]=max(c[j],c[i]+1);
+    }
+  }
+  ll maxx=-1;
+  f(n)
+  {
+    if(c[i]+b[i]-1>maxx)
+      maxx=c[i]+b[i]-1;
+  }
+  cout<<maxx<<endl;
+ }
+
+ return 0;
 }
