ea3470984fcf378476408a878568fe6a68cc3652
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-02 19:21:51 +0530

    Update Minimum coins to get total.cpp

--- Minimum coins to get total.cpp
+++ Minimum coins to get total.cpp
@@ -76,3 +76,68 @@ https://codeforces.com/problemset/problem/1037/A
 https://codeforces.com/problemset/problem/160/A
 https://codeforces.com/problemset/problem/725/E
 */
+
+/*
+#include <bits/stdc++.h>
+#define mem(dp,a) memset(dp,a,sizeof(dp))
+#define pb(x) push_back(x)
+#define m_p(x,y) make_pair(x,y)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
+#define f(n) for(ll i=0;i<n;i++)
+#define r(n) for(ll j=0;j<n;j++)
+#define F first
+#define S second
+#define pi 3.14159265359
+#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
+using namespace std;
+typedef long long int ll;
+ll HRX=1e18;
+ll INF=1e9+7;
+
+void minncoins(ll coins[],ll total,ll n)
+{
+ ll k[n+1][total+1];
+ for(ll i=0;i<=n;i++)
+  k[i][0]=0;
+ for(ll i=1;i<=total;i++)
+  k[0][i]=HRX;
+ for(ll i=1;i<=n;i++)
+ {
+  for(ll j=1;j<=total;j++)
+  {
+    
+    if(j>=coins[i-1])
+     k[i][j]=min(k[i-1][j],1+k[i][j-coins[i-1]]);
+    else
+     k[i][j]=k[i-1][j];
+
+  }
+ }
+ if(k[n][total]==HRX)
+   cout<<"-1"<<endl;
+ else
+   cout<<k[n][total]<<endl;
+}
+
+int main()
+{
+ hs;
+ ll t;
+ cin>>t;
+ f(t)
+ {
+  ll n,total;
+  cin>>total>>n;
+  ll coins[n];
+  f(n)
+  {
+    cin>>coins[i];
+  }
+ // sort(coins,coins+n);
+ 
+  minncoins(coins,total,n);
+ }
+ return 0;
+}
+*/
