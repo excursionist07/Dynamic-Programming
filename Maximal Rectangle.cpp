443ce73d8bb21b5c415730bee4d5bc8e37e8cd3b
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-03 21:23:36 +0530

    Update Maximal Rectangle.cpp

--- Maximal Rectangle.cpp
+++ Maximal Rectangle.cpp
@@ -1,101 +1,68 @@
-#include<bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define all(v) (v).begin(),(v).end()
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-typedef long double ld;
-ll ESP=1e18;
-ll INF=1e9+7;
+const int N=1e3+7;
+    int dp[N][N];
 
-const ll N=1e3+7;
-ll a[N][N];
-
-
-ll histoarea(vector<ll>vec)
+class Solution {
+public:
+    
+    
+ int histoarea(vector<int>vec)
 {
- ll n=vec.size();
- stack<ll>stk;
- ll i=0,tp,maxx=0;
+ int n=vec.size();
+ int i=0,tp,maxx=0;
+ stack<int>stk;
  while(i<n)
  {
-  if(stk.empty() || vec[i]>=vec[stk.top()])
+   if(stk.empty() || vec[i]>=vec[stk.top()])
     stk.push(i),i++;
-  else
-  {
-   tp=stk.top();
-   stk.pop();
-   if(stk.empty())
-    maxx=max(maxx,vec[tp]*i);
    else
-    maxx=max(maxx,vec[tp]*(i-stk.top()-1));
-  }
+   {
+     tp=stk.top();
+     stk.pop();
+     if(stk.empty())
+      maxx=max(maxx,vec[tp]*i);
+     else
+      maxx=max(maxx,vec[tp]*(i-stk.top()-1));
+   }
  }
  while(!stk.empty())
  {
-   tp=stk.top();
-   stk.pop();
-   if(stk.empty())
-    maxx=max(maxx,vec[tp]*i);
-   else
-    maxx=max(maxx,vec[tp]*(i-stk.top()-1));
+     tp=stk.top();
+     stk.pop();
+     if(stk.empty())
+      maxx=max(maxx,vec[tp]*i);
+     else
+      maxx=max(maxx,vec[tp]*(i-stk.top()-1));
  }
  return maxx;
 }
-
-ll mxrecarea(ll m,ll n)
-{
- ll dp[m][n];
- for(ll i=0;i<n;i++)
-    dp[0][i]=a[0][i];
- for(ll i=1;i<m;i++)
+    int maximalRectangle(vector<vector<char>>& matrix)
+    {
+     int m=matrix.size();
+     if(m==0)return 0;
+     int n=matrix[0].size();
+     for(int i=0;i<n;i++)
+         dp[0][i]=matrix[0][i]-'0';
+     for(int i=1;i<m;i++)
+     {
+      for(int j=0;j<n;j++)
+      {
+       if(matrix[i][j]=='1')
+           dp[i][j]=1+dp[i-1][j];
+       else
+           dp[i][j]=0;
+      }
+     }
+     int maxx=0;
+     for(int i=0;i<m;i++)
  {
-  for(ll j=0;j<n;j++)
+  vector<int>vec;
+  for(int j=0;j<n;j++)
   {
-   if(a[i][j]==1)
-    dp[i][j]=1+dp[i-1][j];
-   else
-    dp[i][j]=0;
-  }
- }
- ll maxx=0;
- for(ll i=0;i<m;i++)
- {
-  vector<ll>vec;
-  for(ll j=0;j<n;j++)
-  {
-   vec.pb(dp[i][j]);
+    vec.push_back(dp[i][j]);
   }
   maxx=max(maxx,histoarea(vec));
  }
  return maxx;
-}
-
-int main()
-{
- hs;
- ll t;
- cin>>t;
- f(t)
- {
-  ll m,n;
-  cin>>m>>n;
-  f(m)
-  {
-   r(n)
-   {
-    cin>>a[i][j];
-   }
-  }
-  cout<<mxrecarea(m,n)<<"\n";
-
- }
- return 0;
-}
+     
+    }
+};
