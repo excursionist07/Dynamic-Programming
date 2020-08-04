43c8bffa071d8f05816c4ebe893f1322aabcef1c
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-04 19:55:27 +0530

    Update Max Sum without Adjacents.cpp

--- Max Sum without Adjacents.cpp
+++ Max Sum without Adjacents.cpp
@@ -1,55 +1,21 @@
-#include<bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define all(v) (v).begin(),(v).end()
-#define ff first
-#define ss second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-typedef long double ld;
-ll ESP=1e18;
-ll INF=1e9+7;
+//HOUSE ROBBER I
 
-int main()
-{
- hs;
- ll t;
- cin>>t;
- f(t)
- {
-  ll n;
-  cin>>n;
-  ll a[n];
-  f(n)
-   cin>>a[i];
-  ll dp[n];
-  if(n==0)
-  {
-   cout<<"0"<<"\n";
-   continue;
-  }
-  if(n==1)
-  {
-   cout<<a[0]<<"\n";
-   continue;
-  }
-  if(n==2)
-  {
-   cout<<max(a[0],a[1])<<"\n";
-   continue;
-  }
-  dp[0]=a[0];
-  dp[1]=max(a[0],a[1]);
-  for(ll i=2;i<n;i++)
-    dp[i]=max(dp[i-1],a[i]+dp[i-2]);
-  cout<<dp[n-1]<<"\n";
- }
- return 0;
-
-
-}
+class Solution {
+public:
+    int rob(vector<int>& nums) 
+    {
+     int n=nums.size();
+     if(n==0)
+         return 0;
+     if(n==1)
+         return nums[0];
+     if(n==2)
+         return max(nums[0],nums[1]);
+     int dp[n];
+     dp[0]=nums[0];
+     dp[1]=max(nums[0],nums[1]);
+     for(int i=2;i<n;i++)
+         dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
+     return dp[n-1];
+    }
+};
