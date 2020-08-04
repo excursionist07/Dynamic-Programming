d94138d68eadffcd8b1f95f29c2b9ddb6e346cb5
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-04 14:27:41 +0530

    Update coin change problem.cpp

--- coin change problem.cpp
+++ coin change problem.cpp
@@ -1,136 +1,33 @@
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof dp)
-#define INF 1000000007
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repb(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-
-ll coinschange(ll coins[],ll n,ll total)
-{
-
-	ll k[n][total+1];
-	f(n)
-	{
-	    k[i][0]=1;
-	}
-	for(ll i=1;i<total+1;i++)
+class Solution {
+public:
+    int coinchange(vector<int>&coins,int total)
     {
-        if(i%coins[0]==0)
-            k[0][i]=1;
-        else
-            k[0][i]=0;
+     int n=coins.size();
+     int dp[n+1][total+1];
+     for(int i=0;i<=n;i++)
+         dp[i][0]=1;
+     for(int i=1;i<=total;i++)
+         dp[0][i]=0;
+     for(int i=1;i<=n;i++)
+     {
+      for(int j=1;j<=total;j++)
+      {
+       if(j>=coins[i-1])
+           dp[i][j]=dp[i-1][j]+dp[i][j-coins[i-1]];
+       else
+           dp[i][j]=dp[i-1][j];
+      }
+     }
+     return dp[n][total];
     }
-
-	for(ll i=1;i<n;i++)
-
-	{
-		for(ll j=1;j<total+1;j++)
-		{
-			if(j>=coins[i])
-				k[i][j]=k[i-1][j]+k[i][j-coins[i]];
-			else
-				k[i][j]=k[i-1][j];
-
-		}
-	}
-	return k[n-1][total];
-}
-
-int main()
-{
-	hs;
-	ll n;
-	cin>>n;
-	ll coins[n];
-	f(n)
-	{
-		cin>>coins[i];
-	}
-	sort(coins,coins+n);
-	ll total;
-	cin>>total;
-	cout<<coinschange(coins,n,total)<<endl;
-	return 0;
-
-}
-
-/*
-https://practice.geeksforgeeks.org/problems/coin-change/0
-https://codeforces.com/problemset/problem/433/A
-https://www.spoj.com/problems/EINST/
-https://www.spoj.com/problems/COINS/
-https://www.spoj.com/problems/TPC07/
-*/
-
-/*
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof dp)
-#define INF 1000000007
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repb(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-
-ll coinschange(ll coins[],ll n,ll total)
-{
-
-	ll k[n+1][total+1];
-	f(n)
-	 k[i][0]=1;
-	for(ll i=1;i<=total;i++)
-	 k[0][i]=0;
-	
-	/*for(ll i=1;i<total+1;i++)
+    int change(int amount, vector<int>& coins) 
     {
-        if(i%coins[0]==0)
-            k[0][i]=1;
-        else
-            k[0][i]=0;
-    }*/
-
-	for(ll i=1;i<=n;i++)
-
-	{
-		for(ll j=1;j<=total;j++)
-		{
-			if(j>=coins[i-1])
-				k[i][j]=k[i-1][j]+k[i][j-coins[i-1]];
-			else
-				k[i][j]=k[i-1][j];
-
-		}
-	}
-	return k[n][total];
-}
-
-int main()
-{
-	hs;
-	ll t;
-	cin>>t;
-	while(t--)
-    {
-	ll n;
-	cin>>n;
-	ll coins[n];
-	f(n)
-	{
-		cin>>coins[i];
-	}
-	//sort(coins,coins+n);
-	ll total;
-	cin>>total;
-	cout<<coinschange(coins,n,total)<<endl;
-	
+     int n=coins.size();
+     if(amount==0)
+         return 1;
+     if(n==0)
+         return 0;
+     
+     return coinchange(coins,amount);
     }
-	return 0;
-
-}
-*/
+};
