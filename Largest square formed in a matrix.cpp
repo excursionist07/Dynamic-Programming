0dedb0f8c9b563f3ec4f689b5197fb25d6bf427d
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-05 13:39:10 +0530

    Update Largest square formed in a matrix.cpp

--- Largest square formed in a matrix.cpp
+++ Largest square formed in a matrix.cpp
@@ -1,71 +1,35 @@
-#include<bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define all(v) (v).begin(),(v).end()
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-typedef long double ld;
-ll ESP=1e18;
-ll INF=1e9+7;
-
-const ll N=1e3+7;
-ll a[N][N];
-
-ll maxxsqrsize(ll m,ll n)
-{
- ll dp[m][n];
- ll maxx=0;
- dp[0][0]=a[0][0];
- for(ll i=1;i<n;i++)
-    dp[0][i]=a[0][i];
- for(ll i=1;i<m;i++)
-    dp[i][0]=a[i][0];
-
- for(ll i=1;i<m;i++)
- {
-  for(ll j=1;j<n;j++)
-  {
-   if(a[i][j]==1)
-    dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
-   else
-    dp[i][j]=0;
-  }
- }
- f(m)
- {
-  r(n)
-  {
-   maxx=max(maxx,dp[i][j]);
-  }
- }
- return maxx;
-}
-
-int main()
-{
- hs;
- ll t;
- cin>>t;
- f(t)
- {
-  ll m,n;
-  cin>>m>>n;
-  f(m)
-  {
-   r(n)
-   {
-    cin>>a[i][j];
-   }
-  }
-  cout<<maxxsqrsize(m,n)<<"\n";
-
- }
- return 0;
-}
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix)
+    {
+     int m=matrix.size();
+     if(m==0)return 0;
+     int n=matrix[0].size();
+     int dp[m][n];
+     dp[0][0]=matrix[0][0]-'0';
+    // cout<<dp[0][0]<<"\n";
+     for(int i=1;i<n;i++)
+         dp[0][i]=matrix[0][i]-'0';
+     for(int i=1;i<m;i++)
+         dp[i][0]=matrix[i][0]-'0';
+     for(int i=1;i<m;i++)
+     {
+      for(int j=1;j<n;j++)
+      {
+       if(matrix[i][j]=='1')
+           dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+       else
+           dp[i][j]=0;
+      }
+     }
+     int maxx=0;
+     for(int i=0;i<m;i++)
+     {
+      for(int j=0;j<n;j++)
+      {
+       maxx=max(maxx,dp[i][j]);
+      }
+     }
+        return maxx*maxx;
+    }
+};
