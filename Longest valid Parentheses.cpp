556e296dd29dc205fa58c86271b7557466239f16
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-05 12:05:14 +0530

    Update Longest valid Parentheses.cpp

--- Longest valid Parentheses.cpp
+++ Longest valid Parentheses.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
-    int longestValidParentheses(string s)
+    int longestValidParentheses(string s) 
     {
      int n=s.length();
-     if(n==0)return 0;
+     if(n<=1)
+         return 0;
      int maxx=0;
      stack<int>stk;
      stk.push(-1);
@@ -13,13 +14,15 @@ public:
           stk.push(i);
       else
       {
-        stk.pop();
-        if(!stk.empty())
-            maxx=max(maxx,i-stk.top());
-        else
-            stk.push(i);
+       stk.pop();
+       if(!stk.empty())
+           maxx=max(maxx,i-stk.top());
+       else
+           stk.push(i);
       }
      }
-        return maxx;
+     return maxx;
+     
+     
     }
 };
