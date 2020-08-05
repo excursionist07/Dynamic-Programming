8c2bc5543d69883d59fc7b2d81058e4c0d0e12a4
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-05 14:50:17 +0530

    Update Maximum sum Rectangle.cpp

--- Maximum sum Rectangle.cpp
+++ Maximum sum Rectangle.cpp
@@ -3,86 +3,89 @@
 #define pb(x) push_back(x)
 #define m_p(x,y) make_pair(x,y)
 #define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
+#define per(i,b,a) for (ll i=b;i>=a;i--)
+#define all(v) (v).begin(),(v).end()
+#define ff first
+#define ss second
+#define pi acosl(-1.0l)
 #define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fixed(n) fixed<<setprecision(n)
 using namespace std;
-typedef  long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
-
-ll up,down;
-ll maxx_sum;
-ll minn,start,endd;
-
-
-void kadane(ll temp[],ll m)
-{
- ll maxx=0,s=0;
- minn=INT_MIN,start=0,endd=0;
- for(ll i=0;i<m;i++)
+typedef long long int ll;
+typedef long double ld;
+ll ESP=1e18;
+ll FCB=1e9+7;
+ll dir[][2]={{0,1},{0,-1},{1,0},{-1,0}};
+/*
+ freopen("in.txt","r",stdin);
+ freopen("out.txt","w",stdout);
+ */
+ ll up,down,start,endd;
+ 
+ ll kadane(ll temp[],ll n)
  {
-  maxx=maxx+temp[i];
-  if(maxx<0)
+  ll maxx=-ESP,s=0,idx=0,cur=0;
+  start=0;
+  endd=0;
+  while(idx<n)
   {
-   maxx=0;
-   s=i+1;
+   cur+=temp[idx];
+   if(cur>maxx)
+   {
+    maxx=cur;
+    start=s;
+    endd=idx;
+   }
+   if(cur<0)
+   {
+    cur=0;
+    s=idx+1;
+   }
+   idx++;
   }
-  if(maxx>minn)
-  {
-   minn=max(maxx,minn);
-   start=s;
-   endd=i;
-  }
+  return maxx;
  }
-
-}
-int main()
-{
- hs;
- ll t;
- cin>>t;
- f(t)
+ 
+ int main()
  {
-  ll m,n;
-  cin>>m>>n;
-  ll a[m][n];
-  f(m)
+  hs;
+  ll t;
+  cin>>t;
+  while(t--)
   {
-   r(n)
+   ll m,n;
+   cin>>m>>n;
+   ll a[m][n];
+   rep(i,0,m)
    {
-    cin>>a[i][j];
+    rep(j,0,n)
+    {
+     cin>>a[i][j];
+    }
    }
+   ll maxx=-ESP;
+   ll temp[m],left,right;
+   for(ll i=0;i<n;i++)
+   {
+    mem(temp,0);
+    for(ll j=i;j<n;j++)
+    {
+     for(ll k=0;k<m;k++)
+       temp[k]+=a[k][j];
+     ll zz=kadane(temp,m);
+     if(zz>maxx)
+     {
+      maxx=zz;
+      left=i;
+      right=j;
+      up=start;
+      down=endd;
+     }
+    }
+   }
+   cout<<maxx<<"\n";
+   //cout<<left<<" "<<right<<" "<<up<<" "<<down<<"\n";
+   
   }
-  ll temp[m],left,right;
-  maxx_sum=INT_MIN;
-  for(ll i=0;i<n;i++)
-  {
-   mem(temp,0);
-   for(ll j=i;j<n;j++)
-   {
-    for(ll k=0;k<m;k++)
-        temp[k]+=a[k][j];
-    kadane(temp,m);
-    if(minn>maxx_sum)
-   {
-    maxx_sum=minn;
-    up=start;
-    down=endd;
-    left=i;
-    right=j;
-   }
-   }
-  }
-  cout<<maxx_sum<<endl;
-  cout<<left<<" "<<right<<" "<<up<<" "<<down<<endl;
-
+  return 0;
  }
- return 0;
-
-
-}
