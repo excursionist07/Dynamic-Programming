43c997bdb0401bc961b5b06b383098c10be1ef48
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-05 15:09:36 +0530

    Update Ugly Numbers.cpp

--- Ugly Numbers.cpp
+++ Ugly Numbers.cpp
@@ -1,64 +1,61 @@
-#include<bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
+// UGLY NUMBER I
 
-void uglyno(ll n)
-{
- ll ugly[n];
- ll aa=2;
- ll bb=3;
- ll cc=5;
- ll i2=0;
- ll i3=0;
- ll i5=0;
- ll nun=1;
- ugly[0]=1;
- for(ll i=1;i<n;i++)
- {
-  nun=min(aa,min(bb,cc));
-  ugly[i]=nun;
-  if(nun==aa)
-  {
-    i2=i2+1;
-    aa=(ugly[i2])*2;
-  }
-  if(nun==bb)
-  {
-    i3=i3+1;
-    bb=(ugly[i3])*3;
-  }
-  if(nun==cc)
-  {
-    i5=i5+1;
-    cc=(ugly[i5])*5;
-  }
- }
- cout<<nun<<endl;
-}
-int main()
-{
- hs;
- ll t;
- cin>>t;
- f(t)
- {
-  ll n;
-  cin>>n;
-  uglyno(n);
- }
- return 0;
+class Solution {
+public:
+    bool isUgly(int num)
+    {
+     if(num==1 || num==2 || num==3 || num==5)
+         return true;
+     while(num && num%2==0)
+         num/=2;
+     while(num && num%3==0)
+         num/=3;
+     while(num && num%5==0)
+         num/=5;
+     return (num==1);
+    }
+};
 
-}
+// UGLY NUMBER II
+
+class Solution {
+public:
+    int nthUglyNumber(int n) 
+    {
+     if(n==1)
+         return 1;
+     int dp[n];
+     dp[0]=1;
+     int aa=2,bb=3,cc=5,i2=0,i3=0,i5=0;
+     for(int i=1;i<n;i++)
+     {
+      int minn=min(aa,min(bb,cc));
+      dp[i]=minn;
+      if(minn==aa)
+      {
+       i2++;
+       aa=dp[i2]*2;
+      }
+      if(minn==bb)
+      {
+       i3++;
+       bb=dp[i3]*3;
+      }
+      if(minn==cc)
+      {
+       i5++;
+       cc=dp[i5]*5;
+      }
+       
+      
+     }
+     return dp[n-1];
+     
+    }
+};
+
+/*
+(1) 1×2, 2×2, 3×2, 4×2, 5×2, …
+(2) 1×3, 2×3, 3×3, 4×3, 5×3, …
+(3) 1×5, 2×5, 3×5, 4×5, 5×5, …
+*/
