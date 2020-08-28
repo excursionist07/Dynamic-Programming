cf1635868e518a3f216857957cb0337025dd5fb4
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-29 01:12:46 +0530

    Update 198. House Robber.cpp

--- 198. House Robber.cpp
+++ 198. House Robber.cpp
@@ -49,17 +49,7 @@ public:
 
 // 337. House Robber III
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     int doit(TreeNode* root,int& l,int& r)
