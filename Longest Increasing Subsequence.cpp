e742db4558b20e17054f1aa11e8ebbb7b28c6fed
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-09-15 17:46:59 +0530

    Update Longest Increasing Subsequence.cpp

--- Longest Increasing Subsequence.cpp
+++ Longest Increasing Subsequence.cpp
@@ -50,12 +50,28 @@ int main()
 
 class Solution {
 public:
+    int bs(vector<int>&ans,int lo,int hi,int x) // lower_bound using binary search
+    {
+     while(lo<=hi)
+     {
+      int mid=(lo+hi)/2;
+      if(ans[mid]==x)
+          hi=mid-1;
+      else if(ans[mid]>x)
+          hi=mid-1;
+      else
+          lo=mid+1;
+     }
+     return max(0,lo);
+    }
     int lengthOfLIS(vector<int>& nums)
     {
      vector<int>ans;
      for(int i=0;i<nums.size();i++)
      {
-      int idx=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+      //int idx=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+	     // OR
+      int idx=bs(ans,0,ans.size()-1,nums[i]);
       if(idx==ans.size())
          ans.push_back(nums[i]);
       else
