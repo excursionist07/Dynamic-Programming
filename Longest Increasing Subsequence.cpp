5e3230b2ce9774a383078ed6a7158d377f9bb6cf
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-09-15 17:21:51 +0530

    Update Longest Increasing Subsequence.cpp

--- Longest Increasing Subsequence.cpp
+++ Longest Increasing Subsequence.cpp
@@ -46,6 +46,25 @@ int main()
 	return 0;
 }
 
+// n*log(n)
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums)
+    {
+     vector<int>ans;
+     for(int i=0;i<nums.size();i++)
+     {
+      int idx=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+      if(idx==ans.size())
+         ans.push_back(nums[i]);
+      else
+          ans[idx]=nums[i];
+     }
+     return ans.size();
+    }
+};
+
 /*
 https://practice.geeksforgeeks.org/problems/longest-increasing-subsequence/0
 https://www.hackerrank.com/challenges/longest-increasing-subsequent/problem
