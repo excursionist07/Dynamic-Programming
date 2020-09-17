3bff100c03414c35f419fd443a842be977fae635
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-09-17 14:12:00 +0530

    Update Longest Palindromic Substring.cpp

--- Longest Palindromic Substring.cpp
+++ Longest Palindromic Substring.cpp
@@ -50,7 +50,7 @@ public:
     }
 };
 
-// o(n*n) && o(n) --> expanding around the center
+// o(n*n) && o(n) --> expanding around the center (trivial algo used in manachers)
 
 class Solution {
 public:
@@ -76,29 +76,78 @@ public:
       
      }
     
-     int maxx1=0,maxx2=0,idx1=-1,idx2=-1;
+     int l=-1,r=-1;
+     int len=0;
      for(int i=0;i<n;i++)
      {
-      if(2*d1[i]-1>maxx1)
+      if(2*d1[i]-1>len)
+          len=2*d1[i]-1,l=i-(d1[i]-1),r=i+(d1[i]-1);
+      if(2*d2[i]>len)
+          len=2*d2[i],l=i-d2[i],r=i+d2[i]-1;
+     }
+     return s.substr(l,len);
+     
+    }
+};
+
+// o(n) && o(n)--> Manachers's algo
+
+class Solution {
+public:
+    string longestPalindrome(string s) 
+    {
+     int n=s.length();
+     if(n<2)
+         return s;
+     vector<int>d1(n),d2(n);
+     int l=0,r=-1;
+     
+     // for odd size
+     for(int i=0;i<n;i++)
+     {
+      if(r>=i)
+          d1[i]=min(r-i+1,d1[l+r-i]); // We'll try to extract some information from the already calculated values in d1[]. So, let's find the "mirror" position of i in the sub-palindrome (l,r), i.e. we'll get the position j=l+(r−i)
+      else
+          d1[i]=1;
+      // trivial algo
+      while(i-d1[i]>=0 && 1+d1[i]<n && s[i-d1[i]]==s[i+d1[i]])
+          d1[i]++;
+      if(i+d1[i]-1>r)
       {
-       maxx1=2*d1[i]-1;
-       idx1=i;
+       r=i+d1[i]-1;
+       l=i-(d1[i]-1);
       }
      }
         
+     // for even size
+     l=0;r=-1;
      for(int i=0;i<n;i++)
      {
-      if(2*d2[i]>maxx2)
+      if(r>=i)
+          d2[i]=min(r-i+1,d2[l+r-i+1]);
+      else
+          d2[i]=0;
+      // // trivial algo
+      while(i-d2[i]-1>=0 && i+d2[i]<n && s[i-d2[i]-1]==s[i+d2[i]])
+          d2[i]++;
+      if(i+d2[i]-1>r)
       {
-       maxx2=2*d2[i];
-       idx2=i;
+       r=i+d2[i]-1;
+       l=i-d2[i];
       }
+      
      }
-     if(maxx1>maxx2)
-         return s.substr(idx1-maxx1/2,maxx1/2)+s[idx1]+s.substr(idx1+1,maxx1/2);
-     else
-         return s.substr(idx2-maxx2/2,maxx2/2)+s.substr(idx2,maxx2/2);
-     
+     l=-1;r=-1;
+     int len=0;
+     for(int i=0;i<n;i++)
+     {
+      if(2*d1[i]-1>len) // odd length
+          len=2*d1[i]-1,l=i-(d1[i]-1),r=i+(d1[i]-1);
+      if(2*d2[i]>len) // even length
+          len=2*d2[i],l=i-d2[i],r=i+d2[i]-1;
+     }
+     return s.substr(l,len);
+        
     }
 };
 
