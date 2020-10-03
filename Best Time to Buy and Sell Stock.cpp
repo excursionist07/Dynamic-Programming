fe54e1b067c4ba8860a9dc377e04c6d143422713
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-10-04 02:21:30 +0530

    Update Best Time to Buy and Sell Stock.cpp

--- Best Time to Buy and Sell Stock.cpp
+++ Best Time to Buy and Sell Stock.cpp
@@ -154,10 +154,11 @@ public:
       buy[0]=-prices[0];
       sell[1]=buy[0]+prices[1];
       buy[1]=-prices[1];
-      sell[2]=max(buy[1]+prices[2],sell[1]-prices[1]+prices[2]);
-      buy[2]=-prices[2];
-      int maxx=max(sell[2],max(sell[1],0));
-      for(int i=3;i<n;i++)
+      //sell[2]=max(buy[1]+prices[2],sell[1]-prices[1]+prices[2]);
+      //buy[2]=-prices[2];
+     // int maxx=max(sell[2],max(sell[1],0));
+       int maxx=max(sell[1],0);
+      for(int i=2;i<n;i++)
       {
        sell[i]=max(buy[i-1]+prices[i],sell[i-1]-prices[i-1]+prices[i]);
        buy[i]=max(sell[i-2]-prices[i],buy[i-1]+prices[i-1]-prices[i]);
