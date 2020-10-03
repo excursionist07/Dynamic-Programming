e220c525c333951b638b1fb5f2a7ee2ed0861dc7
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-10-04 00:34:26 +0530

    Update 0-1 Knapsack Problem.cpp

--- 0-1 Knapsack Problem.cpp
+++ 0-1 Knapsack Problem.cpp
@@ -1,3 +1,6 @@
+// 0-1 Knapsack Problem
+
+
 #include <bits/stdc++.h>
 #define mem(dp,a) memset(dp,a,sizeof dp)
 #define INF 1000000007
@@ -54,4 +57,64 @@ https://www.hackerearth.com/problem/algorithm/knapsack-1/
 https://www.codechef.com/problems/KNPSK
 http://codeforces.com/problemset/gymProblem/101064/L
 https://codeforces.com/problemset/problem/19/B
+*/
+
+
+// unbounded_knapsack
+
+
+// same as rod cutting
+
+#include <bits/stdc++.h>
+#define mem(dp,a) memset(dp,a,sizeof(dp))
+#define pb(x) push_back(x)
+#define m_p(x,y) make_pair(x,y)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
+#define f(n) for(ll i=0;i<n;i++)
+#define r(n) for(ll j=0;j<n;j++)
+#define F first
+#define S second
+#define pi 3.14159265359
+#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
+using namespace std;
+typedef long long int ll;
+ll HRX=1e18;
+ll INF=1e9+7;
+
+void unbounded_kanpsack(ll wt[],ll val[],ll n,ll total)
+{
+  ll k[n+1][total+1];
+  f(n+1)
+  {
+    r(total+1)
+    {
+      if(i==0 || j==0)
+        k[i][j]=0;
+      else if(j>=wt[i-1])
+        k[i][j]=max(k[i-1][j],val[i-1]+k[i][j-wt[i-1]]);
+      else
+        k[i][j]=k[i-1][j];
+    }
+  }
+  cout<<k[n][total]<<endl;
+}
+int main()
+{
+  hs;
+  ll n;
+  cin>>n;
+  ll wt[n],val[n];
+  f(n)
+  {
+    cin>>wt[i]>>val[i];
+  }
+  ll total;
+  cin>>total;
+  unbounded_kanpsack(wt,val,n,total);
+  return 0;
+}
+
+/*
+https://practice.geeksforgeeks.org/problems/knapsack-with-duplicate-items/0
 */
