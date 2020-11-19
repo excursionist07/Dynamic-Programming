dcc677700c5cb63e5a61f9a512c85764aab81dba
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-11-19 14:13:50 +0530

    Create Painter's Partition Problem.cpp

---
+++ Painter's Partition Problem.cpp
@@ -0,0 +1,48 @@
+// Painter's Partition Problem(IB)
+
+
+bool IsValid(vector<int>&C,int mid,int n)
+{
+ long long int painter=1,cur=0;
+ for(int i=0;i<C.size();i++)
+ {
+  if(cur+C[i]>mid)
+  {
+   cur=C[i];
+   painter++;
+   if(painter>n)
+    return false;
+  }
+  else
+   cur+=C[i];
+ }
+ return true;
+}
+long long int bs(vector<int>&C,int n)
+{
+ long long int start=-1,end=0;
+ for(int i=0;i<C.size();i++)
+ {
+  end+=C[i];
+  start=max(start,(long long int)C[i]);
+ }
+ long long int ans=0;
+ while(start<=end)
+ {
+  int mid=(start+end)/2;
+  if(IsValid(C,mid,n))
+  {
+    ans=mid;
+    end=mid-1;
+  }
+  else
+   start=mid+1;
+ }
+ return ans;
+}
+
+int Solution::paint(int A, int B, vector<int> &C) 
+{
+  long long int zz=bs(C,A);
+  return ((zz%10000003)*(B%10000003))%10000003;
+}
