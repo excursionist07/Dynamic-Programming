ba1fcb0af8e178be24fbea0931eab11311f3c1de
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-11-19 14:22:11 +0530

    Update Painter's Partition Problem.cpp

--- Painter's Partition Problem.cpp
+++ Painter's Partition Problem.cpp
@@ -46,3 +46,54 @@ int Solution::paint(int A, int B, vector<int> &C)  // O(n*log(n))
   long long int zz=bs(C,A);
   return ((zz%10000003)*(B%10000003))%10000003;
 }
+
+// 410. Split Array Largest Sum
+
+class Solution {
+public:
+    bool IsValid(vector<int>&nums,int mid,int n)
+    {
+     int cur=0,painter=1;
+     for(int i=0;i<nums.size();i++)
+     {
+      if(cur+nums[i]>mid)
+      {
+       cur=nums[i];
+       painter++;
+       if(painter>n)
+           return false;
+      }
+      else
+          cur+=nums[i];
+     }
+     return true;
+    }
+    int bs(vector<int>&nums,int n)
+    {
+     int start=-1,end=0;
+     for(int i=0;i<nums.size();i++)
+     {
+      end+=nums[i];
+      start=max(start,nums[i]);
+     }
+     int ans=0;
+     while(start<=end)
+     {
+      int mid=(start+end)>>1;
+      if(IsValid(nums,mid,n))
+      {
+       ans=mid;
+       end=mid-1;
+      }
+      else
+          start=mid+1;
+     }
+     return ans;
+    }
+    int splitArray(vector<int>& nums, int m)
+    {
+     int n=nums.size();
+     int ans=bs(nums,m);
+     return ans;
+    }
+};
