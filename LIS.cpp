35f21cc83092d665ae14fceb059eecb5fa0d95a5
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2021-03-31 14:00:01 +0530

    Update LIS.cpp

--- LIS.cpp
+++ LIS.cpp
@@ -1,52 +1,23 @@
 // LIS
 
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof dp)
-#define INF 1000000007
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repb(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-
-
-ll lislength(ll a[],ll n)
-{
-	ll k[n];
-	f(n)
-	{
-		k[i]=1;
-	}
-	for(ll j=1;j<n;j++)
-	{
-		for(ll i=0;i<j;i++)
-		{
-			if(a[i]<a[j])
-				k[j]=max(k[j],k[i]+1);
-		}
-	}
-	return *max_element(k,k+n);
-}
-int main()
-{
-	hs;
-	ll t;
-	cin>>t;
-	f(t)
-	{
-	ll n;
-	cin>>n;
-	ll a[n];
-	f(n)
-	{
-		cin>>a[i];
-	}
-	cout<<lislength(a,n)<<endl;
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) 
+    {
+      int n=nums.size();
+      vector<int>dp(n,1);
+      for(int j=1;j<n;j++)
+      {
+       for(int i=0;i<j;i++)
+       {
+        if(nums[j]>nums[i])
+            dp[j]=max(dp[j],1+dp[i]);
+       }
+      }
+      return *max_element(dp.begin(),dp.end());
+     
     }
-	return 0;
-}
+};
 
 // n*log(n)
 
@@ -433,7 +404,8 @@ public:
     }
 };
 
-// 1671. Minimum Number of Removals to Make Mountain Array
+// 1671. Minimum Number of Removals to Make Mountain Array (Like Longest Bitonic subsequence)
+
 //  // O(n*log(n))  
 class Solution {
 public:
