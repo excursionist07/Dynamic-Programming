9ed0ec75f50c11ca1112cdf44fef43e81686c0c2
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2021-05-15 08:26:05 +0530

    Update Jump Game.cpp

--- Jump Game.cpp
+++ Jump Game.cpp
@@ -1,68 +1,30 @@
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
+// 55. Jump Game
 
-
-int main()
-{
- hs;
- ll t;
- cin>>t;
- f(t)
- {
-  ll n;
-  cin>>n;
-  ll a[n];
-  f(n)
-  {
-    cin>>a[i];
-  }
-  if(a[0]==0)
-  {
-   cout<<"INFINITY"<<endl;
-   return 0;
-  }
-
-  ll b[n],c[n];
-  b[0]=0;
-  for(ll i=1;i<n;i++)
-    b[i]=INF;
-  c[0]=-1;
-  for(ll j=1;j<n;j++)
-  {
-    for(ll i=0;i<j;i++)
+class Solution {
+public:
+    bool canJump(vector<int>& nums)
     {
-      if(i+a[i]>=j)
+     int n=nums.size();
+     if(n<2)
+         return true;
+     if(nums[0]==0)return false;
+     vector<long>dp(n,INT_MAX);
+     dp[0]=0;
+     for(int j=1;j<n;j++)
+     {
+      for(int i=0;i<j;i++)
       {
-        b[j]=min(b[j],b[i]+1);
-        c[j]=i;
-        break;
-
+       if(i+nums[i]>=j)
+       {
+         dp[j]=min(dp[j],1+dp[i]);
+         break;
+       }
+       
       }
+     }
+     return (dp[n-1]>=INT_MAX ? 0 : 1);
     }
-  }
-  cout<<b[n-1]<<endl;
-  f(n)
-  {
-    cout<<c[i]<<" ";
-  }
-  cout<<"\n";
- }
- return 0;
-}
+};
 
 
 
