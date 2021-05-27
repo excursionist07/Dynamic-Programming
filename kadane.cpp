354df2e6222c8aae6dc3ec1947c666a8daefac6a
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2021-05-27 12:35:59 +0530

    Update kadane.cpp

--- kadane.cpp
+++ kadane.cpp
@@ -29,3 +29,77 @@ public:
 
     }
 };
+
+// 918. Maximum Sum Circular Subarray
+
+class Solution {
+public:
+    int kadaneMax(vector<int>& nums)
+    {
+     int n=nums.size();
+     int mx=INT_MIN;
+     for(int i=0;i<n;i++)
+         mx=max(mx,nums[i]);
+     int cur=0,i=0;
+     while(i<n)
+     {
+      cur+=nums[i];
+     
+      if(cur>mx)
+         mx=cur;
+      if(cur<0)
+        cur=0;
+      i++;
+     }
+     return mx;
+    }
+    
+    int kadaneMin(vector<int>& nums)
+    {
+     int n=nums.size();
+     int mn=INT_MAX;
+     for(int i=0;i<n;i++)
+         mn=min(mn,nums[i]);
+     int cur=0,i=0;
+     while(i<n)
+     {
+      cur+=nums[i];
+      if(cur<mn)
+         mn=cur;
+      if(cur>0)
+        cur=0;
+      i++;
+     }
+     return mn;
+    }
+    
+    int maxSubarraySumCircular(vector<int>& nums) 
+    {
+     int n=nums.size();
+     int total=0,mx=INT_MIN;
+     bool positive=0;
+     for(int i=0;i<n;i++)
+     {
+      total+=nums[i];
+      mx=max(mx,nums[i]);
+      if(nums[i]>=0)
+          positive=1;
+     }
+         
+     int ans1=kadaneMax(nums); // normal kadane in straight array
+     int ans2=kadaneMin(nums); // if suffix+prefix makes max
+     if(positive)
+         return max(ans1,total-ans2);
+     return mx; // if all elements are negative
+     
+     
+    }
+};
+
+/*
+--> proof for total-ans2
+max(prefix+suffix)
+= max(total sum - subarray)
+= total sum + max(-subarray)
+= total sum - min(subarray)
+*/
