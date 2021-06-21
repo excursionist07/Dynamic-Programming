401608a9e092bfb4d4d3ecf18ca5251e0042143f
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2021-06-21 14:13:17 +0530

    Update Longest Palindromic Substring.cpp

--- Longest Palindromic Substring.cpp
+++ Longest Palindromic Substring.cpp
@@ -192,6 +192,48 @@ public:
     }
 };
 
+// 131. Palindrome Partitioning
+
+class Solution {
+public:
+    bool IsPal(string s,int start,int end)
+    {
+     while(start<=end)
+     {
+      if(s[start++]!=s[end--])
+          return false;
+     }
+     return true;
+    }
+    void doit(string s,vector<string>temp,vector<vector<string>>&ans,int idx)
+    {
+     if(idx==s.length())
+     {
+      ans.push_back(temp);
+      return;
+     }
+     for(int i=idx;i<s.length();i++)
+     {
+      if(IsPal(s,idx,i))
+      {
+       temp.push_back(s.substr(idx,i-idx+1));
+       doit(s,temp,ans,i+1);
+       temp.pop_back();
+      }
+     }
+    }
+    vector<vector<string>> partition(string s) 
+    {
+     vector<vector<string>>ans;
+     int n=s.length();
+     if(n==0)
+         return ans;
+     vector<string>temp;
+     doit(s,temp,ans,0);
+     return ans;
+    }
+};
+
 // 132.Palindrome Partioning(min cut)
 
 class Solution {
